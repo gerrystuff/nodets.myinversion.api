8103fcc4f135b375fdd0dde4bb1befb6e8a6889c
gerry <luisgerardoaguilarpadilla@gmail.com>
2021-05-21 06:04:29 -0600

    deploy server to heroku

--- dist/database/database.js
+++ dist/database/database.js
@@ -2,21 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 //produccion
-// const database = new Sequelize(
-//     'heroku_caf616ba1ae2085', //database
-//     'b1e7faacb53c5a', //user
-//     '91872644', //password
-//         {
-//             host: 'us-cdbr-east-03.cleardb.com', //host
-//             dialect: 'mysql'        }
-//     );
-//desarrollo
-const database = new sequelize_1.Sequelize('myinversion', //database
-'root', //user
-'Redcode010', //password
+const database = new sequelize_1.Sequelize('heroku_caf616ba1ae2085', //database
+'b1e7faacb53c5a', //user
+'91872644', //password
 {
-    host: 'localhost',
+    host: 'us-cdbr-east-03.cleardb.com',
     dialect: 'mysql'
 });
+//desarrollo
 exports.default = database;
 //# sourceMappingURL=database.js.map
